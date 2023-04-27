0a67d80e671b4af990b7120bfb33d35905616648
DrWhats <hudorojckov.leva@gmail.com>
2023-04-27 21:50:12 +0500

    DVC test3

--- main.py
+++ main.py
@@ -3,8 +3,17 @@ import pandas as pd
 # Загрузка данных из файла
 train_data = pd.read_csv('data/train.csv')
 
+print(train_data.head())
+
 # Удаление дубликатов
 train_data = train_data.drop_duplicates()
+
+print(train_data.shape)
+
+train_data = train_data[["Pclass", "Sex", "Age"]]
+
+print(train_data.shape)
+print(train_data.head())
 
 # Перезапись файла с новыми данными
 train_data.to_csv('data/train.csv', index=False)
