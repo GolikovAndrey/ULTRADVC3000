fbaa1f03ad6a7c904f5b98d43b682979de1caa07
GolikovAndrey <and.golikoff@yandex.ru>
2023-04-29 16:04:34 +0500

    Transform Sex column into onehotencoded values

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+from sklearn.preprocessing import OneHotEncoder
 # Загрузка данных из файла
 train_data = pd.read_csv('data/train.csv')
 
@@ -12,10 +12,14 @@ print(train_data.shape)
 
 train_data = train_data[["Pclass", "Sex", "Age"]]
 
-train_data['Age'] = train_data.Age.fillna(train_data.Age.mean())
+train_data['Age'] = train_data['Age'].fillna(train_data['Age'].mean())
 
 print(train_data.shape)
 print(train_data.head())
 
-# Перезапись файла с новыми данными
+ohe = OneHotEncoder(drop='first', handle_unknown='ignore', sparse_output=False)
+train_data['Sex'] = ohe.fit_transform(train_data[['Sex']])
+
+print(train_data)
+
 train_data.to_csv('data/train.csv', index=False)
